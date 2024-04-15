4951a4988e769e5d5e55dee07d5de3e43e4e39da
skllll06 <sk0413.jp@gmail.com>
2024-04-16 00:28:05 +0900

    install drizzle

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,10 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { UsersModule } from './users/users.module';
+import { DrizzleModule } from './drizzle/drizzle.module';
 
 @Module({
-  imports: [UsersModule],
+  imports: [UsersModule, DrizzleModule],
   controllers: [AppController],
   providers: [AppService],
 })
